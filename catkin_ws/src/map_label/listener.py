51c25b978e9592282b89fca0bd8c427abe109db8
TaiChunYen <kevintai0304@gmail.com>
2019-08-08 12:18:14 +0800

    get depth

--- catkin_ws/src/map_label/listener.py
+++ catkin_ws/src/map_label/listener.py
@@ -2,11 +2,28 @@ import rospy
 import pdb
 import sys
 sys.path.append("/home/taita/catkin_ws/src/darknet_ros")
-#pdb.set_trace()
+
 from darknet_ros_msgs.msg import *
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+from sensor_msgs.msg import Image
 
 def callback(datas):
     rospy.loginfo(datas.bounding_boxes[0].probability)
+
+def callback_depth(depth_data):
+    bridge = CvBridge()
+    try:
+        depth_image = bridge.imgmsg_to_cv2(depth_data, "32FC1")
+    except CvBridgeError as e:
+        print(e)
+
+    depth_array = np.array(depth_image, dtype=np.float32)
+    print('Image size: {width}x{height}'.format(width=depth_data.width,height=depth_data.height))
+    
+    u = depth_data.width/2
+    v = depth_data.height/2
+    print('Center depth: {dist} m'.format(dist=depth_array[u,v]))
     
 def listener():
 
@@ -18,6 +35,8 @@ def listener():
     rospy.init_node('listener', anonymous=True)
 
     rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, callback)
+
+    rospy.Subscriber("/zed_node/depth/depth_registered", Image, callback_depth)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
