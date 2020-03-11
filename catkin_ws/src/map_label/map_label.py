9f3598daa4e5e87357399589dea24ce96b4f0dc1
TaiChunYen <kevintai0304@gmail.com>
2020-03-11 19:43:44 +0800

    can label marker position on .jpg map file

--- catkin_ws/src/map_label/map_label.py
+++ catkin_ws/src/map_label/map_label.py
@@ -1,10 +1,14 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('map.jpg')
+height = 217
+map_c_x = int((-1.6777+4.681169)/0.05)
+map_c_y = height - int((0.0255478+3.636700)/0.05)
 
-cv2.circle(img,(157, 124), 3, (0, 0, 255), -1)
+img = cv2.imread('passion.jpg')
+cv2.circle(img,(map_c_x, map_c_y), 3, (0, 0, 255), -1)
 
 cv2.imshow('My Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+cv2.waitKey(1)
