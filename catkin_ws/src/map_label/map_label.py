bf5028fac226a4f37c36de0e3720b2e3079e4555
TaiChunYen <kevintai0304@gmail.com>
2020-03-28 21:56:33 +0800

    can map different markers with different colors on map

--- catkin_ws/src/map_label/map_label.py
+++ catkin_ws/src/map_label/map_label.py
@@ -11,17 +11,17 @@ for line in lines:
     marker_set.append(marker)
 
 ##### DBSCAN #####
-'''
+
 np_marker_set = np.array(marker_set).astype(np.float64)
 #print(np_marker_set.shape)#(marker num,2)
 clustering = DBSCAN(eps=0.2, min_samples=1).fit(np_marker_set)
 labels = clustering.labels_
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 #print(n_clusters_)
-'''
+
 ##### show marker cluster result plot #####
-'''
-core_samples_mask = np.zeros_like(labels, dtype=bool)
+
+'''core_samples_mask = np.zeros_like(labels, dtype=bool)
 core_samples_mask[clustering.core_sample_indices_] = True
 import matplotlib.pyplot as plt
 unique_labels = set(labels)
@@ -43,10 +43,39 @@ for k, col in zip(unique_labels, colors):
              markeredgecolor='k', markersize=6)
 
 plt.title('Estimated number of clusters: %d' % n_clusters_)
-plt.show()
-'''
-##### show marker on map #####
+plt.show()'''
+
+##### marker filter #####
+
+label_cursor = 0
+marker_filter = []
+for i in range(len(labels)):
+    if labels[i]==label_cursor:
+        marker_filter.append(i)
+        label_cursor+=1
+    
+filted_marker = [marker_set[index] for index in marker_filter]
+
+##### show different marker on map #####
 height = 217
+img = cv2.imread('passion.jpg')
+origin_x = -4.681169
+origin_y = -3.636700
+
+for point in filted_marker:
+    map_c_x = int((float(point[0])-origin_x)/0.05)
+    map_c_y = height - int((float(point[1])-origin_y)/0.05)
+    
+    color_plate = (list(np.random.choice(range(256), size=3)))
+    cv2.circle(img,(map_c_x, map_c_y), 3, (int(color_plate[0]), int(color_plate[1]), int(color_plate[2])), -1)
+
+cv2.imshow('My Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.waitKey(1)
+
+##### show marker on map #####
+'''height = 217
 img = cv2.imread('passion.jpg')
 origin_x = -4.681169
 origin_y = -3.636700
@@ -60,4 +89,4 @@ for point in marker_set:
 cv2.imshow('My Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-cv2.waitKey(1)
+cv2.waitKey(1)'''
